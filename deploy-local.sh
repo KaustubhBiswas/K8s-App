8b33686fad4979402b44a8daccc0f2a6fa6dbe0c
Kaustubh <kaustubhbiswas001@gmail.com>
2025-08-08 12:45:18 +0530

    Test pipeline update

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 set -e
 
-# Variables
 DOCKER_USERNAME="kaustubhbiswas"
-TAG=$(curl -s https://hub.docker.com/v2/repositories/$DOCKER_USERNAME/node-app/tags/ \
-      | jq -r '.results[0].name')
 
-# Deploy to local Minikube
+echo "Fetching latest commit SHA from Docker Hub..."
+TAG=$(curl -s https://hub.docker.com/v2/repositories/$DOCKER_USERNAME/node-app/tags/ \
+  | grep -o '"name":"[a-f0-9]\{40\}"' \
+  | head -n 1 \
+  | cut -d':' -f2 \
+  | tr -d '"')
+
+if [ -z "$TAG" ]; then
+  echo "❌ Could not fetch latest tag from Docker Hub!"
+  exit 1
+fi
+
+echo "Latest tag is: $TAG"
+echo "Deploying to local Minikube..."
+
 kubectl set image deployment/node-app node-app=$DOCKER_USERNAME/node-app:$TAG
 kubectl rollout status deployment/node-app
